0c30737874f0f9330d503f38f9f3d9b6c63ea61b
Tomas <33667251+tvancisin@users.noreply.github.com>
2025-06-02 19:39:10 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -424,7 +424,7 @@
 
     <ol>
       <li>
-        <strong>Distant Mediation Fosters Agreements</strong>: The study finds
+        <strong>Distant Mediation Fosters Agreements</strong>: The study finds blaaaaaaaa
         that the further a mediation event is from a conflict area, the higher
         the likelihood of a formal peace agreement being signed. This suggests
         that a greater geographical distance can confer perceived neutrality,
