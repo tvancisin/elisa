f5c3ca0813a32836e96acd74d8753b8ad4fa5d07
Elisa D'Amico <127250270+elisadamico@users.noreply.github.com>
2025-06-06 11:41:31 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -453,7 +453,7 @@
         indicates that for every additional “far away” mediation event, the odds
         of a peace agreement being signed increase by approximately 14.2%. This
         underscores the strategic value of neutral ground when aiming for formal
-        settlements. Hello I'm writing. 
+        settlements.
       </li>
     </ol>
   </div>
